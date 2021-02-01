3ac9965f855d3f5d5423b4e47a04eeb4c95d2ab2
Brandon Cheng <brandon.cheng@protonmail.com>
2021-01-31 20:48:56 -0500

    Promote get module upward
    The userspace client will share `get` types with the DKMS client.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #[cfg(target_os = "linux")]
 pub mod linux;
 #[cfg(target_os = "linux")]
+pub use linux::{err, set, DeviceInterface, RouteSocket, WgSocket};
+
+pub mod get;
 pub use linux::{err, get, set, DeviceInterface, RouteSocket, WgSocket};
