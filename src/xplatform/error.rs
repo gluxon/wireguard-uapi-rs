9472de3ef195ac311cb6674a8d5a619ca7a21ec9
Brandon Cheng <brandon.cheng@protonmail.com>
2021-01-31 20:52:58 -0500

    Implement xplatform `set` command

--- src/xplatform/error.rs
+++ src/xplatform/error.rs
@@ -7,3 +7,15 @@ pub enum GetDeviceError {
     #[error(transparent)]
     ParseGetDevice(#[from] ParseGetResponseError),
 }
+
+#[derive(Debug, thiserror::Error)]
+pub enum SetDeviceError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("Received non-zero error number in response: `{0}`")]
+    ServerError(String),
+    #[error("Received empty response")]
+    EmptyResponse,
+    #[error("Failed to parse response: `{0}`")]
+    InvalidResponse(String),
+}
